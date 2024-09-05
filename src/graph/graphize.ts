ed855a02ba6be58558d095f7d7e3bf9b0b63d0d8
KiMDLE <68802037+kimdeul@users.noreply.github.com>
2024-09-05 20:34:04 +0900

    Make upline-stop connect with downline-stop

--- src/graph/graphize.ts
+++ src/graph/graphize.ts
@@ -18,40 +18,17 @@ export function graphize() {
         for (const stop of route.list.slice(1)) {
             if (!graph.has(`${stop}`)) graph.set(`${stop}`, [])
             const [from, to] = [prev, stop].map(id => BUS_STOPS.find(s => s.number === id))
-            if (from && to && from.number && to.number) {
-                graph.get(`${from.number}`)?.push({ to: to.number, weight: dist(from, to)})
-                graph.get(`${to.number}`)?.push({ to: from.number, weight: dist(from, to)})
-            }
+            if (from && to && from.number && to.number) graph.get(`${from.number}`)?.push({ to: to.number, weight: dist(from, to)})
             prev = stop;
         }
     })
 
-    // BUS_STOPS.map(from => {
-    //     const to = BUS_STOPS.find(stop => stop.number === BUS_STOPS.filter(to => to.name === from.name)?.[0].number)
-    //     if (!to || !to.number || !from.number) return;
-    //     graph.get(`${to.number}`)?.push({ to: from.number, weight: dist(from, to)})
-    //     graph.get(`${from.number}`)?.push({ to: to.number, weight: dist(from, to)})
-    // })
-
-    // import sys
-    // from math import inf
-
-    // N = int(input())
-    // M = int(input())
-
-    // graph = [[inf] * N for _ in range(N)]
-    // for _ in range(M):
-    //     a, b, weight = map(int, sys.stdin.readline().split())
-    //     graph[a-1][b-1] = min(weight, graph[a-1][b-1])
-
-    // for transitP in range(N):
-    //     for fromP in range(N):
-    //         for toP in range(N):
-    //             if fromP == toP: continue
-    //             graph[fromP][toP] = min(graph[fromP][toP], graph[fromP][transitP]+graph[transitP][toP])
-
-    // for i in graph:
-    //     print(' '.join(map(str, i)).replace('inf', '0'))
+    BUS_STOPS.map(from => {
+        const to = BUS_STOPS.find(stop => stop.number === BUS_STOPS.filter(to => to.name === from.name && dist(from, to) <= 500)?.[0].number)
+        if (!to || !to.number || !from.number) return;
+        graph.get(`${to.number}`)?.push({ to: from.number, weight: dist(from, to)})
+        graph.get(`${from.number}`)?.push({ to: to.number, weight: dist(from, to)})
+    })
 
     console.log("Graphize completed.")
 
